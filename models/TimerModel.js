5f60196aaedc5381fe99cc10dd2f5942b0d3992c
Eric Petersen <eric@huskers.unl.edu>
2013-06-29 16:37:42 -0500

    adding var declaration after use strict

--- models/TimerModel.js
+++ models/TimerModel.js
@@ -2,7 +2,7 @@
 
 ArZeit.service('TimerModel',
     function(){
-        _timers = [
+        var _timers = [
                 {id: 0, title: 'test timer 1', active: false},
                 {id: 1, title: 'test timer 2', active: false},
                 {id: 2, title: 'test timer 3', active: false},
