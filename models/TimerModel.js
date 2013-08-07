b35abb70301ec697e0739a534246f9eb632e9e02
Eric Petersen <eric@huskers.unl.edu>
2013-08-07 14:28:37 -0500

    hooking Timer model and views up to arzeit_server backend

--- models/TimerModel.js
+++ models/TimerModel.js
@@ -2,29 +2,16 @@
 
 ArZeit.service('TimerModel',
     function($http){
-        var _timers = [
-            {id: 0, title: 'test timer 1', active: false},
-            {id: 1, title: 'test timer 2', active: false},
-            {id: 2, title: 'test timer 3', active: false},
-        ];
-
-        var timer_pk_regex = /timers\/(\d+)\//g;
+        var _timers = [];
 
         function loadTimers(data, status, headers, config){
-            for (var i; i < data.length; i++){
-                var timer_data = data[i];
-                var timer_id = timer_pk_regex.exec(data.url)[1];
-                var timer = {
-                    id: timer_id,
-                    title: timer_data.name,
-                    active: timer_data.active,
-                };
-                _timers.push(timer);
+            for (var i = 0; i < data.length; i++){
+                _timers.push(data[i]);
             }
         }
 
         this.getTimers = function(){
-            $http.get('http://localhost:8000/timers/').success(loadTimers);
+            $http.get('http://127.0.0.1/api/timers/').success(loadTimers);
             // cross origin policy violated even for different ports...
             return _timers;
         };
