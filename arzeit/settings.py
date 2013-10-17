ae46e9777490a9187dee04a7bc9034bd813b89ca
Eric Petersen <eric@huskers.unl.edu>
2013-10-17 17:57:44 -0500

    updating settings and adding base template view

--- arzeit/settings.py
+++ arzeit/settings.py
@@ -1,4 +1,7 @@
 # Django settings for arzeit project.
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -54,6 +57,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(CURRENT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -89,6 +93,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(CURRENT_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
